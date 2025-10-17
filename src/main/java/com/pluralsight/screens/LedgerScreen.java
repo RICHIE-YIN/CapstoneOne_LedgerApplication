0b077ae95853e70390d25d6e58caed7e82a51e90
Richie <richiesyin@icloud.com>
2025-10-17 11:11:07 -0700

    Added some customizations (ascii art)

--- src/main/java/com/pluralsight/screens/LedgerScreen.java
+++ src/main/java/com/pluralsight/screens/LedgerScreen.java
@@ -12,13 +12,6 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class LedgerScreen {
-    public static void main(String[] args) {
-
-//        viewDeposits();
-        viewPayments();
-//        viewAll();
-
-    }
 
     static Scanner scanner = new Scanner(System.in);
     static ArrayList<Ledger> ledger = new ArrayList<>();
@@ -26,11 +19,18 @@ public class LedgerScreen {
     static LocalTime currentTime = LocalTime.now().withNano(0);
 
     public static void ledgerScreen(){
+        String art = """
+                   __          __               ____                  \s
+                  / /  ___ ___/ /__ ____ ____  / __/__________ ___ ___\s
+                 / /__/ -_) _  / _ `/ -_) __/ _\\ \\/ __/ __/ -_) -_) _ \\
+                /____/\\__/\\_,_/\\_, /\\__/_/   /___/\\__/_/  \\__/\\__/_//_/
+                              /___/                                   \s
+                """;
+        System.out.println(art);
         System.out.println("A) All\n" +
                 "D) Deposits\n" +
                 "P) Payments\n" +
                 "R) Reports");
-        System.out.println("REMINDER TO SELF YOU NEED TO DISPLAY BACKWARDS");
 
         String answer = scanner.nextLine();
         if(answer.equalsIgnoreCase("a")) {
@@ -63,6 +63,7 @@ public class LedgerScreen {
                 ledger.add(temp);
 //                System.out.printf("%s %s %s %s %.2f \n", temp.getDate(), temp.getTime(), temp.getDescription(), temp.getVendor(), temp.getAmount());
             }
+            bufferedReader.close();
 
             for(int i = ledger.size() - 1; i >= 0; i--) {
                 Ledger current = ledger.get(i);
@@ -78,21 +79,23 @@ public class LedgerScreen {
 
     public static void viewDeposits() {
         try{
-        FileReader fileReader = new FileReader("src/main/java/com/pluralsight/data/transactions.csv");
-        BufferedReader bufferedReader = new BufferedReader(fileReader);
-        ledger.clear();
+            FileReader fileReader = new FileReader("src/main/java/com/pluralsight/data/transactions.csv");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            ledger.clear();
 
-        String input;
-        while((input = bufferedReader.readLine()) != null) {
-            String trimmedInput = input.trim();
-            String [] data = trimmedInput.split("\\|");
-            Ledger temp = new Ledger(data[2], data[3], Double.parseDouble(data[4]));
-            temp.setDate(LocalDate.parse(data[0]));
-            temp.setTime(LocalTime.parse(data[1]));
-            ledger.add(temp);
+            String input;
+            while((input = bufferedReader.readLine()) != null) {
+                String trimmedInput = input.trim();
+                String [] data = trimmedInput.split("\\|");
+                Ledger temp = new Ledger(data[2], data[3], Double.parseDouble(data[4]));
+                temp.setDate(LocalDate.parse(data[0]));
+                temp.setTime(LocalTime.parse(data[1]));
+                ledger.add(temp);
 
 //            System.out.printf("%s %s %s %s %.2f \n", temp.getDate(), temp.getTime(), temp.getDescription(), temp.getVendor(), temp.getAmount());
-        }
+            }
+            bufferedReader.close();
+
             for(int i = ledger.size() - 1; i >= 0; i--) {
                 Ledger current = ledger.get(i);
                 if(current.getAmount() > 0) {
@@ -122,6 +125,8 @@ public class LedgerScreen {
                 temp.setTime(LocalTime.parse(data[1]));
                 ledger.add(temp);
             }
+            bufferedReader.close();
+
             for(int i = ledger.size() - 1; i >= 0; i--) {
                 Ledger current = ledger.get(i);
                 if(current.getAmount() <= 0) {
--- src/main/java/com/pluralsight/screens/Reports.java
+++ src/main/java/com/pluralsight/screens/Reports.java
@@ -5,21 +5,24 @@ import com.pluralsight.objects.Ledger;
 import java.io.*;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Reports {
 
-    public static void main(String[] args) {
-//        monthToDate();
-//        yearToDate();
-//        previousMonth();
-//        previousYear();
-//        searchByVendor("test2");
-
-    }
-
     static Scanner scanner = new Scanner(System.in);
+    static ArrayList<Ledger> ledger = new ArrayList<>();
+
+
     public static void reportsScreen() {
+        String art = """
+                   ___                    __        ____                  \s
+                  / _ \\___ ___  ___  ____/ /____   / __/__________ ___ ___\s
+                 / , _/ -_) _ \\/ _ \\/ __/ __(_-<  _\\ \\/ __/ __/ -_) -_) _ \\
+                /_/|_|\\__/ .__/\\___/_/  \\__/___/ /___/\\__/_/  \\__/\\__/_//_/
+                        /_/                                               \s
+                """;
+        System.out.println(art);
         System.out.println("1) Month To Date\n" +
                 "2) Previous Month\n" +
                 "3) Year To Date\n" +
@@ -58,25 +61,36 @@ public class Reports {
         int currentYear = currentDate.getYear();
 
         try{
-        FileReader fileReader = new FileReader("src/main/java/com/pluralsight/data/transactions.csv");
-        BufferedReader bufferedReader = new BufferedReader(fileReader);
-        String input;
+            FileReader fileReader = new FileReader("src/main/java/com/pluralsight/data/transactions.csv");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            ledger.clear();
+            String input;
+            boolean notEmpty = false;
 
-        while((input = bufferedReader.readLine()) != null) {
-            String trimmedInput = input.trim();
-            String [] data = trimmedInput.split("\\|");
-            String [] dateSplit = data[0].split("-");
-            int year = Integer.parseInt(dateSplit[0]);
-            int month = Integer.parseInt(dateSplit[1]);
-            if(year == currentYear && month == currentMonth) {
-                Ledger filteredList = new Ledger(data[2], data[3], Double.parseDouble(data[4]));
-                filteredList.setDate(LocalDate.parse(data[0]));
-                filteredList.setTime(LocalTime.parse(data[1]));
-                System.out.printf("%s %s %s %s %.2f \n", filteredList.getDate(), filteredList.getTime(), filteredList.getDescription(), filteredList.getVendor(), filteredList.getAmount());
+            while((input = bufferedReader.readLine()) != null) {
+                String trimmedInput = input.trim();
+                String [] data = trimmedInput.split("\\|");
+                String [] dateSplit = data[0].split("-");
+                int year = Integer.parseInt(dateSplit[0]);
+                int month = Integer.parseInt(dateSplit[1]);
+                if(year == currentYear && month == currentMonth) {
+                    Ledger filteredList = new Ledger(data[2], data[3], Double.parseDouble(data[4]));
+                    filteredList.setDate(LocalDate.parse(data[0]));
+                    filteredList.setTime(LocalTime.parse(data[1]));
+                    ledger.add(filteredList);
+                    notEmpty = true;
+                }
             }
+            bufferedReader.close();
 
-
-        }
+            if(notEmpty) {
+                for(int i = ledger.size() - 1; i >= 0; i--) {
+                    Ledger current = ledger.get(i);
+                    System.out.printf("%s %s %s %s %.2f \n", current.getDate(), current.getTime(), current.getDescription(), current.getVendor(), current.getAmount());
+                }
+            } else {
+                System.out.println("Nothing found, sorry.");
+            }
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         } catch (IOException e) {
@@ -89,11 +103,19 @@ public class Reports {
         LocalDate currentDate = LocalDate.now();
         int currentMonth = currentDate.getMonthValue();
         int currentYear = currentDate.getYear();
+        int targetMonth = currentMonth - 1;
+        int targetYear = currentYear;
+        if(targetMonth == 0) {
+            targetMonth = 12;
+            targetYear--;
+        }
 
         try{
             FileReader fileReader = new FileReader("src/main/java/com/pluralsight/data/transactions.csv");
             BufferedReader bufferedReader = new BufferedReader(fileReader);
+            ledger.clear();
             String input;
+            boolean notEmpty = false;
 
             while((input = bufferedReader.readLine()) != null) {
                 String trimmedInput = input.trim();
@@ -101,14 +123,24 @@ public class Reports {
                 String [] dateSplit = data[0].split("-");
                 int year = Integer.parseInt(dateSplit[0]);
                 int month = Integer.parseInt(dateSplit[1]);
-                if(year == currentYear && month == currentMonth - 1) {
+                if(year == targetYear && month == targetMonth) {
                     Ledger filteredList = new Ledger(data[2], data[3], Double.parseDouble(data[4]));
                     filteredList.setDate(LocalDate.parse(data[0]));
                     filteredList.setTime(LocalTime.parse(data[1]));
-                    System.out.printf("%s %s %s %s %.2f \n", filteredList.getDate(), filteredList.getTime(), filteredList.getDescription(), filteredList.getVendor(), filteredList.getAmount());
+                    ledger.add(filteredList);
+                    notEmpty = true;
                 }
 
+            }
+            bufferedReader.close();
 
+            if(notEmpty) {
+                for(int i = ledger.size() - 1; i >= 0; i--) {
+                    Ledger current = ledger.get(i);
+                    System.out.printf("%s %s %s %s %.2f \n", current.getDate(), current.getTime(), current.getDescription(), current.getVendor(), current.getAmount());
+                }
+            } else {
+                System.out.println("Nothing found, sorry.");
             }
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
@@ -125,7 +157,9 @@ public class Reports {
         try{
             FileReader fileReader = new FileReader("src/main/java/com/pluralsight/data/transactions.csv");
             BufferedReader bufferedReader = new BufferedReader(fileReader);
+            ledger.clear();
             String input;
+            boolean notEmpty = false;
 
             while((input = bufferedReader.readLine()) != null) {
                 String trimmedInput = input.trim();
@@ -137,10 +171,19 @@ public class Reports {
                     Ledger filteredList = new Ledger(data[2], data[3], Double.parseDouble(data[4]));
                     filteredList.setDate(LocalDate.parse(data[0]));
                     filteredList.setTime(LocalTime.parse(data[1]));
-                    System.out.printf("%s %s %s %s %.2f \n", filteredList.getDate(), filteredList.getTime(), filteredList.getDescription(), filteredList.getVendor(), filteredList.getAmount());
+                    ledger.add(filteredList);
+                    notEmpty = true;
                 }
+            }
+            bufferedReader.close();
 
-
+            if(notEmpty) {
+                for(int i = ledger.size() - 1; i >= 0; i--) {
+                    Ledger current = ledger.get(i);
+                    System.out.printf("%s %s %s %s %.2f \n", current.getDate(), current.getTime(), current.getDescription(), current.getVendor(), current.getAmount());
+                }
+            } else {
+                System.out.println("Nothing found, sorry.");
             }
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
@@ -157,7 +200,9 @@ public class Reports {
         try{
             FileReader fileReader = new FileReader("src/main/java/com/pluralsight/data/transactions.csv");
             BufferedReader bufferedReader = new BufferedReader(fileReader);
+            ledger.clear();
             String input;
+            boolean notEmpty = false;
 
             while((input = bufferedReader.readLine()) != null) {
                 String trimmedInput = input.trim();
@@ -169,10 +214,19 @@ public class Reports {
                     Ledger filteredList = new Ledger(data[2], data[3], Double.parseDouble(data[4]));
                     filteredList.setDate(LocalDate.parse(data[0]));
                     filteredList.setTime(LocalTime.parse(data[1]));
-                    System.out.printf("%s %s %s %s %.2f \n", filteredList.getDate(), filteredList.getTime(), filteredList.getDescription(), filteredList.getVendor(), filteredList.getAmount());
+                    ledger.add(filteredList);
+                    notEmpty = true;
                 }
+            }
+            bufferedReader.close();
 
-
+            if(notEmpty) {
+                for(int i = ledger.size() - 1; i >= 0; i--) {
+                    Ledger current = ledger.get(i);
+                    System.out.printf("%s %s %s %s %.2f \n", current.getDate(), current.getTime(), current.getDescription(), current.getVendor(), current.getAmount());
+                }
+            } else {
+                System.out.println("Nothing found, sorry.");
             }
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
@@ -184,21 +238,34 @@ public class Reports {
 
     public static void searchByVendor(String vendorName) {
         try{
-        FileReader fileReader = new FileReader("src/main/java/com/pluralsight/data/transactions.csv");
-        BufferedReader bufferedReader = new BufferedReader(fileReader);
-        String input;
+            FileReader fileReader = new FileReader("src/main/java/com/pluralsight/data/transactions.csv");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            ledger.clear();
+            String input;
+            boolean notEmpty = false;
 
-        while((input = bufferedReader.readLine()) != null) {
-            String trimmedInput = input.trim();
-            String [] data = trimmedInput.split("\\|");
-            String vendor = data[3];
-            if(vendor.equalsIgnoreCase(vendorName)) {
-                Ledger filteredList = new Ledger(data[2], data[3], Double.parseDouble(data[4]));
-                filteredList.setDate(LocalDate.parse(data[0]));
-                filteredList.setTime(LocalTime.parse(data[1]));
-                System.out.printf("%s %s %s %s %.2f \n", filteredList.getDate(), filteredList.getTime(), filteredList.getDescription(), filteredList.getVendor(), filteredList.getAmount());
+            while((input = bufferedReader.readLine()) != null) {
+                String trimmedInput = input.trim();
+                String [] data = trimmedInput.split("\\|");
+                String vendor = data[3];
+                if(vendor.equalsIgnoreCase(vendorName)) {
+                    Ledger filteredList = new Ledger(data[2], data[3], Double.parseDouble(data[4]));
+                    filteredList.setDate(LocalDate.parse(data[0]));
+                    filteredList.setTime(LocalTime.parse(data[1]));
+                    ledger.add(filteredList);
+                    notEmpty = true;
+                }
             }
-        }
+            bufferedReader.close();
+
+            if(notEmpty) {
+                for(int i = ledger.size() - 1; i >= 0; i--) {
+                    Ledger current = ledger.get(i);
+                    System.out.printf("%s %s %s %s %.2f \n", current.getDate(), current.getTime(), current.getDescription(), current.getVendor(), current.getAmount());
+                }
+            } else {
+                System.out.println("Vendor not found. Try again.");
+            }
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         } catch (IOException e) {
